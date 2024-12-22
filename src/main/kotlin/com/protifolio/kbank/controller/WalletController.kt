32ee10ffad0f14c8e57f694af4ee623bac910fdc
ProjetosEltonLira <eltondelira@gmail.com>
2024-12-22 12:25:10 -0300

    feat: incluindo a primeira versão do ExceptionHandler na controler

--- src/main/kotlin/com/protifolio/kbank/controller/WalletController.kt
+++ src/main/kotlin/com/protifolio/kbank/controller/WalletController.kt
@@ -1,8 +1,13 @@
 package com.protifolio.kbank.controller
 
 import com.protifolio.kbank.controller.dto.CreateWalletDto
+import com.protifolio.kbank.exception.WalletDataAlreadyExistsException
 import com.protifolio.kbank.service.WalletService
+import org.springframework.http.HttpStatus
+import org.springframework.http.HttpStatusCode
+import org.springframework.http.ProblemDetail
 import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
@@ -19,5 +24,13 @@ class WalletController (private var walletService: WalletService){
         var wallet = walletService.createWallet(dto)
 
         return ResponseEntity.created(URI.create("/wallets/" + wallet.walletId.toString())).build()
+    }
+
+    @ExceptionHandler(WalletDataAlreadyExistsException::class)
+    fun handleWalletDataAlreadyExistsException (e : WalletDataAlreadyExistsException): ProblemDetail {
+        return ProblemDetail.forStatus(422).apply {
+            title = "Wallet data already exists"
+            detail = e.message
+        }
     }
 }
--- src/main/kotlin/com/protifolio/kbank/exception/WalletDataAlreadyExistsException.kt
+++ src/main/kotlin/com/protifolio/kbank/exception/WalletDataAlreadyExistsException.kt
@@ -1,5 +1,10 @@
 package com.protifolio.kbank.exception
 
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
+
+//Não é a melhor pratica
+//@ResponseStatus(HttpStatus.UNPROCESSABLE_ENTITY)
 class WalletDataAlreadyExistsException : KBankException {
 
     constructor(message: String) : super(message)
